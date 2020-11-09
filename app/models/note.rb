6f2c53cb1c601665fb73b6c895873fdb46a36cbb
chloekim917 <chloekim917@gmail.com>
2020-11-09 15:47:39 -0500

    clean up2

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,10 +1,5 @@
 class Note < ApplicationRecord
     include Rails.application.routes.url_helpers
     belongs_to :notebook
-     # has_one_attached :image
-  
-    # def get_image_url
-    #     url_for(self.image)
-    # end
    
 end
--- app/serializers/notebook_serializer.rb
+++ app/serializers/notebook_serializer.rb
@@ -1,5 +1,4 @@
 class NotebookSerializer
   include FastJsonapi::ObjectSerializer
   attributes :notebook_name
-  # belongs_to :user
 end
