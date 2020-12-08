814e3615d92438d5b27018755ef1b013257c5f28
chloekim917 <chloekim917@gmail.com>
2020-12-07 19:48:15 -0500

    comment2

--- app/serializers/note_serializer.rb
+++ app/serializers/note_serializer.rb
@@ -1,7 +1,8 @@
 class NoteSerializer
   include FastJsonapi::ObjectSerializer
-
+  
   belongs_to :notebook
+  #wanted to save picture as well but react native version update prevented me from doing it
   attributes ..., :image, 
 
   def image_url
